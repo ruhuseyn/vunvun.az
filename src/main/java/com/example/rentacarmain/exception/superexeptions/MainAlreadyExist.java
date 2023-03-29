4c29369cceebc145b3ffc20cf55b2be7266e5e3f
ruhuseyn <huseyinovrufet49@gmail.com>
2023-03-29 13:40:10 +0400

    added more managers exceptions

---
+++ src/main/java/com/example/rentacarmain/exception/superexeptions/MainAlreadyExist.java
@@ -0,0 +1,8 @@
+package com.example.rentacarmain.exception.superexeptions;
+
+public class MainAlreadyExist extends RuntimeException{
+
+    public MainAlreadyExist(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/rentacarmain/exception/superexeptions/MainNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.rentacarmain.exception.superexeptions;
+
+public class MainNotFoundException extends RuntimeException{
+    public MainNotFoundException(String message) {
+        super(message);
+    }
+}
